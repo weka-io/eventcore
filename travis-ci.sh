671fd0f078c1b9564093ccd5c06a69bb6263cc29
Sönke Ludwig <sludwig@rejectedsoftware.com>
2016-12-19 22:30:15 +0100

    Update test script to take into account the selected test configuration.

--- travis-ci.sh
+++ travis-ci.sh
@@ -3,19 +3,19 @@
 set -e -x -o pipefail
 
 # test for successful release build
-dub build --combined -b release --compiler=$DC
+dub build -b release --compiler=$DC -c $CONFIG
 
 # test for successful 32-bit build
 if [ "$DC" == "dmd" ]; then
-	dub build --combined --arch=x86
+	dub build --arch=x86 -c $CONFIG
 fi
 
-dub test --combined --compiler=$DC
+dub test --compiler=$DC -c $CONFIG
 
 if [ ${BUILD_EXAMPLE=1} -eq 1 ]; then
     for ex in $(\ls -1 examples/*.d); do
         echo "[INFO] Building example $ex"
-        dub build --compiler=$DC --single $ex
+        dub build --compiler=$DC --single $ex # --override-config eventcore/$CONFIG
     done
     rm -rf examples/.dub/
     rm examples/*-example
@@ -23,6 +23,6 @@ fi
 if [ ${RUN_TEST=1} -eq 1 ]; then
     for ex in `\ls -1 tests/*.d`; do
         echo "[INFO] Running test $ex"
-        dub --temp-build --compiler=$DC --single $ex
+        dub --temp-build --compiler=$DC --single $ex # --override-config eventcore/$CONFIG
     done
 fi
